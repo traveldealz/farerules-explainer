5b7f219c779853e37af70dc39e80ef80c10cf358
Johannes Kinast <johannes@kinast.me>
2021-07-08 15:02:24 +0000

    Add advanced_reservation_days, travel_period_from/to

--- src/explainer.js
+++ src/explainer.js
@@ -18,4 +18,59 @@ export default class {
     return date_string;
   }
 
+  get advanced_reservation_days() {
+    let found = this.text.match(/RESERVATIONS FOR ALL SECTORS ARE REQUIRED AT LEAST (\d{1,3}) DAYS/);
+
+    if(null === found || 2 !== found.length) {
+      return null;
+    }
+
+    return parseInt(found[1]);
+
+  }
+
+  get travel_period_from() {
+    return this.#travel_period(/FROM [A-Z ]+ -\n*[ ]*PERMITTED ([A-Z0-9 \n]+) FOR EACH/);
+  }
+
+  get travel_period_to() {
+    console.log(this.#travel_period(/TO [A-Z ]+ -\n*[ ]*PERMITTED ([A-Z0-9 \n]+) FOR EACH/))
+    return this.#travel_period(/TO [A-Z ]+ -\n*[ ]*PERMITTED ([A-Z0-9 \n]+) FOR EACH/);
+  }
+
+  #travel_period(regex) {
+    let found = this.text.match(regex);
+
+    if(null === found || 2 !== found.length) {
+      return null;
+    }
+
+    //console.log(found);
+
+    let periods = found[1].split('OR').map(period => {
+      let [from, to] = period.split('THROUGH').map(date => {
+        let [,day,month_name] = date.trim().match(/(\d{2})(\w{3})/);
+        return months[month_name] + '-' + day;
+      } );
+      return {from, to}
+    });
+
+    return periods;
+  }
+
+}
+
+const months = {
+  JAN: '01',
+  FEB: '02',
+  MAR: '03',
+  APR: '04',
+  MAY: '05',
+  JUN: '06',
+  JUL: '07',
+  AUG: '08',
+  SEP: '09',
+  OCT: '10',
+  NOV: '11',
+  DEC: '12',
 }
--- test/index.test.js
+++ test/index.test.js
@@ -11,7 +11,10 @@ import * as farerules from './data/farerules.js';
 
 describe("Test Properties", function() {
   Object.entries(farerules).forEach( ([key, {expected, text}]) => {
-    it(`issued_unti at ${key}`, () => expect((new Explainer(text).issued_until)).to.equal(expected.issued_until) );
+    it(`${key}: issued_until`, () => expect((new Explainer(text).issued_until)).to.equal(expected.issued_until ?? null) );
+    it(`${key}: advanced_reservation_days`, () => expect((new Explainer(text).advanced_reservation_days)).to.equal(expected.advanced_reservation_days ?? null) );
+    it(`${key}: travel_period_from`, () => expect((new Explainer(text).travel_period_from)).to.eql(expected.travel_period_from ?? null) );
+    it(`${key}: travel_period_to`, () => expect((new Explainer(text).travel_period_to)).to.eql(expected.travel_period_to ?? null) );
   } );
   
 });
