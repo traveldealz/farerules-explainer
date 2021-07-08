d9cbda0ed122ce061f8a45ed518221ea7b1e3a95
ditmarlange <ditmarlange@hotmail.de>
2021-07-09 00:17:17 +0200

    AY, BA, TK Fare Rule tests

--- test/data/farerules.js
+++ test/data/farerules.js
@@ -2,3 +2,6 @@ export {default as EY_ZRV4DE} from './EY_ZRV4DE.js';
 export {default as IB_ILX8C8S4} from './IB_ILX8C8S4.js';
 export {default as TP_ODEDSITF} from './TP_ODEDSITF.js';
 export {default as TP_T23DSC0H} from './TP_T23DSC0H.js';
+export {default as TK_UFLYXPC} from './TK_UFLYXPC.js';
+export {default as AY_ZNN0S6BZ} from './AY_ZNN0S6BZ.js';
+export {default as BA_SLNC40S5} from './BA_SLNC40S5.js';
--- test/index.test.js
+++ test/index.test.js
@@ -17,6 +17,9 @@ describe("Test Properties", function() {
     if(expected.travel_period_blackout)  it(`${key}: travel_period_blackout`, () => expect((new Explainer(text).travel_period_blackout)).to.eql(expected.travel_period_blackout) );
     if(expected.travel_period_from) it(`${key}: travel_period_from`, () => expect((new Explainer(text).travel_period_from)).to.eql(expected.travel_period_from) );
     if(expected.travel_period_to) it(`${key}: travel_period_to`, () => expect((new Explainer(text).travel_period_to)).to.eql(expected.travel_period_to) );
+    if(expected.min_stay) it(`${key}: min_stay`, () => expect((new Explainer(text).min_stay)).to.eql(expected.min_stay) );
+    if(expected.max_stay) it(`${key}: max_stay`, () => expect((new Explainer(text).max_stay)).to.eql(expected.max_stay) );
+    if(expected.sunday_rule) it(`${key}: sunday_rule`, () => expect((new Explainer(text).sunday_rule)).to.eql(expected.sunday_rule) );
   } );
-  
+
 });
