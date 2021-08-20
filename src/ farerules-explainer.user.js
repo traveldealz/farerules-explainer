11a3ce40a4b6df3912c36671b4f7810c03412ce5
Johannes Kinast <johannes@kinast.me>
2021-08-20 11:03:02 +0000

    Add download/updateURL

--- src/ farerules-explainer.user.js
+++ src/ farerules-explainer.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name     Fare Rules Explainer
 // @namespace fare-rules-explainer
-// @version  0.0.5
+// @version  0.0.6
+// @downloadURL https://github.com/traveldealz/farerules-explainer/blob/main/src/%20farerules-explainer.user.js
+// @updateURL https://github.com/traveldealz/farerules-explainer/blob/main/src/%20farerules-explainer.user.js
 // @include https://matrix.itasoftware.com/*
 // @grant    none
-// @require https://raw.githubusercontent.com/traveldealz/farerules-explainer/gh-pages/dist/explainer.js
+// @require https://gist.githubusercontent.com/goaround/844c1ea9144483c01f7b8f920e8cd65c/raw/d6e8f5be915ad1a5debf9251692ae0518fcf190c/farerules-explainer.js
 // @run-at 				document-idle
 // ==/UserScript==
 
