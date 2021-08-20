b32b1b0173d67713f98a32a7c032e6360854017a
Johannes Kinast <johannes@kinast.me>
2021-08-20 11:05:21 +0000

    Rename

--- src/farerules-explainer.user.js
+++ src/farerules-explainer.user.js
@@ -2,8 +2,8 @@
 // @name     Fare Rules Explainer
 // @namespace fare-rules-explainer
 // @version  0.0.6
-// @downloadURL https://github.com/traveldealz/farerules-explainer/blob/main/src/%20farerules-explainer.user.js
-// @updateURL https://github.com/traveldealz/farerules-explainer/blob/main/src/%20farerules-explainer.user.js
+// @downloadURL https://github.com/traveldealz/farerules-explainer/blob/main/src/farerules-explainer.user.js
+// @updateURL https://github.com/traveldealz/farerules-explainer/blob/main/src/farerules-explainer.user.js
 // @include https://matrix.itasoftware.com/*
 // @grant    none
 // @require https://gist.githubusercontent.com/goaround/844c1ea9144483c01f7b8f920e8cd65c/raw/d6e8f5be915ad1a5debf9251692ae0518fcf190c/farerules-explainer.js
